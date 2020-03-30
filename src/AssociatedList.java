1be7ff4c99dbe1cd6287751ac99fe4536060e588
hhurd <hghurd2001@gmail.com>
2020-03-30 11:14:13 -0400

    Implemented SimpleMap

--- src/AssociatedList.java
+++ src/AssociatedList.java
@@ -1,2 +1,33 @@
-public class AssociatedList {
+import java.util.List;
+
+public class AssociatedList implements SimpleMap{
+    @Override
+    public Integer get(String key) {
+        return null;
+    }
+
+    @Override
+    public Integer put(String key, Integer value) {
+        return null;
+    }
+
+    @Override
+    public Integer remove(String key) {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public List<Entry> entryList() {
+        return null;
+    }
 }
